6a2dfdc824defec6d1e7463fc7f384e35b5d426d
Rivu Khoda <rivu.khoda@gmail.com>
2016-07-11 23:04:59 -0400

    New: All resources can now be deleted in the end point

--- hello.py
+++ hello.py
@@ -2,9 +2,11 @@ from flask import Flask, jsonify, json, request
 from flask_pymongo import PyMongo
 from bson import json_util
 from clarifai.client import ClarifaiApi
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.config.from_object('settings.DevelopmentConfig')
+CORS(app)
 mongo = PyMongo(app)
 clarifai_api = ClarifaiApi()
 
@@ -20,6 +22,12 @@ def get_images(category_of_clothing):
     response = json.dumps(images, default=json_util.default)
     return jsonify(response)
 
+@app.route('/images/delete_all', methods=['DELETE'])
+def delete_all_images():
+    mongo.db.images.delete_many({})
+    return jsonify(status=200, message="success")
+
+
 @app.route('/upload', methods=['POST'])
 def store_image():
     image_metadata = request.get_json()
@@ -30,10 +38,10 @@ def store_image():
 def classify_clothing(image_url):
     categories_of_clothes = ['shirt', 'pants', 'jacket', 'footwear']
     possible_categories = clarifai_api.tag_image_urls(image_url)
-    tags = possible_categories['results'][0]['result']['tag']['classes']
+    generated_classes_of_clothes = possible_categories['results'][0]['result']['tag']['classes']
 
     for category_of_clothing in categories_of_clothes:
-        if category_of_clothing in tags:
+        if category_of_clothing in generated_classes_of_clothes:
             return category_of_clothing
 
 if __name__ == "__main__":
