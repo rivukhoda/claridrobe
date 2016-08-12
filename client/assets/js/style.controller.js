3209a2d945b0cd08620af6c4b43ae9b32d9d3fbd
Rivu Khoda <rivu.khoda@gmail.com>
2016-08-11 23:16:44 -0400

    New: Style Controller separated into a different file

---
+++ client/assets/js/style.controller.js
@@ -0,0 +1,20 @@
+angular
+    .module('app')
+    .controller('StyleController', StyleController);
+
+function StyleController($scope, $http) {
+
+    $scope.date = new Date();
+    $scope.saved = null;
+
+    $http.get(host + 'outfits', {headers: {'Content-Type': 'application/json'}}).then(function successCallback(response) {
+        $scope.savedOutfits = response.data;
+    });
+
+    $scope.deleteOutfit = function () {
+        $scope.savedOutfits.splice(this.$index, 1);
+        var oid = this.outfit._id.$oid;
+        $http.delete(host + 'outfits/' + oid, {headers: {'Content-Type': 'application/json'}});
+    };
+
+}
