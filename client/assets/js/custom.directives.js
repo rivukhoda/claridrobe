8f2350262c7314aacb991ef1a6566e898de47063
Rivu Khoda <rivu.khoda@gmail.com>
2016-08-23 19:20:02 -0400

    New: Custom directive for header is created.

---
+++ client/assets/js/custom.directives.js
@@ -0,0 +1,10 @@
+angular
+    .module('app')
+    .directive('myHeader', function () {
+        return {
+            templateUrl: 'my-header.html',
+            restrict: 'E'
+        }
+    });
+
+
