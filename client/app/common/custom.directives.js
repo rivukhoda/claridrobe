60fb8885d488be99cb4aa9327f8e80952017e7ce
Rivu Khoda <rivu.khoda@gmail.com>
2016-08-29 23:52:41 -0400

    WIP: Fixed Header, Greeter, Footer relative location.

--- client/app/common/custom.directives.js
+++ client/app/common/custom.directives.js
@@ -2,20 +2,20 @@ angular
     .module('app')
     .directive('myHeader', function () {
         return {
-            templateUrl: 'my-header.html',
+            templateUrl: '../common/my-header.html',
             restrict: 'E',
             scope: {title: '@'}
         }
     })
     .directive('myFooter', function () {
         return {
-            templateUrl: 'my-footer.html',
+            templateUrl: '../common/my-footer.html',
             restrict: 'E'
         }
     })
     .directive('myGreeter', function () {
         return {
-            templateUrl: 'my-greeter.html',
+            templateUrl: '../common/my-greeter.html',
             restrict: 'E',
             scope: {greeting: '@'},
             controller: 'GreetingController'
