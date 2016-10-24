5dafae61e54b1fd2078d57e7ebdc782e2bf8b397
Rivu Khoda <rivu.khoda@gmail.com>
2016-10-23 23:48:55 -0400

    Refactor: Changed paths to paths relative to the domain.

--- client/app/common/custom.directives.js
+++ client/app/common/custom.directives.js
@@ -2,20 +2,20 @@ angular
     .module('app')
     .directive('myHeader', function () {
         return {
-            templateUrl: '../common/my-header.html',
+            templateUrl: '/claridrobe/client/app/common/my-header.html',
             restrict: 'E',
             scope: {title: '@'}
         }
     })
     .directive('myFooter', function () {
         return {
-            templateUrl: '../common/my-footer.html',
+            templateUrl: '/claridrobe/client/app/common/my-footer.html',
             restrict: 'E'
         }
     })
     .directive('myGreeter', function () {
         return {
-            templateUrl: '../common/my-greeter.html',
+            templateUrl: '/claridrobe/client/app/common/my-greeter.html',
             restrict: 'E',
             scope: {greeting: '@'},
             controller: 'GreetingController'
