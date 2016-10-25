d7cd57fc0795537f65696158ea6c1a6a01009375
Rivu Khoda <rivu.khoda@gmail.com>
2016-10-24 23:38:16 -0400

    New: SAP Routes configured.

--- client/app/module.js
+++ client/app/module.js
@@ -1,3 +1,31 @@
 angular
-    .module('app', ['toaster', 'ngAnimate'])
-    .constant('config', {'host' : 'http://localhost:5000/'});
+    .module('app', ['toaster', 'ngAnimate', 'ngRoute'])
+    .constant('config', {'host': 'http://localhost:5000/'})
+    .config(function ($routeProvider) {
+        $routeProvider
+
+            .when('/gallery', {
+                templateUrl: 'gallery/gallerytwo.html',
+                controller: 'GalleryController'
+            })
+
+            .when('/outfit', {
+                templateUrl: 'outfit/outfittwo.html',
+                controller: 'OutfitController'
+            })
+
+            .when('/style', {
+                templateUrl: 'style/styletwo.html',
+                controller: 'StyleController'
+            })
+
+            .when('/upload', {
+                templateUrl: 'upload/uploadtwo.html',
+                controller: 'UploadController'
+            })
+
+            .when('/', {
+                templateUrl: 'home/home.html',
+            })
+    });
+
